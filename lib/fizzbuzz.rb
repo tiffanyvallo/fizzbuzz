3180cb3ef1d9b08c958fc3e8e05ea4e57df732a8
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-03-26 11:42:33 +0000

    Fizzbuzz method completed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,15 @@
 def fizzbuzz(number)
-  'fizz'
+  while number <= 99 do
+    number += 1
+    if number % 3 == 0 && number % 5 == 0
+      puts "fizzbuzz"
+    elsif number % 3 == 0
+      puts "fizz"
+    elsif number % 5 == 0
+      puts "buzz"
+    else 
+      puts number
+    end
+  end
 end
+fizzbuzz(0)
